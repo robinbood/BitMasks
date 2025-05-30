78e4c87fb06aa5a32729ed0977f92e4536a4ec14
robinbood <mercedesfarts@gmail.com>
2025-05-30 09:42:30 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [value, setValue] = useState<string>("");
+  const [newData, setNewData] = useState<{ city: string; GMT: string }[]>([]);
+  const [ap, setAp] = useState<boolean>(false);
+  const data: { city: string; GMT: string }[] = [
+    { city: "Moscow", GMT: "+3" },
+    { city: "Paris", GMT: "+2" },
+    { city: "Berlin", GMT: "+2" },
+    { city: "Brussels", GMT: "+2" },
+    { city: "Amsterdam", GMT: "+2" },
+    { city: "Rome", GMT: "+2" },
+    { city: "London", GMT: "+1" },
+    { city: "Dublin", GMT: "+1" },
+    { city: "New York", GMT: "-4" },
+    { city: "Washington", GMT: "-4" },
+    { city: "Los Angeles", GMT: "-7" },
+    { city: "Tokyo", GMT: "+9" },
+    { city: "Beijing", GMT: "+8" },
+    { city: "Mumbai", GMT: "+7" },
+  ];
 
+  const button = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    const filtered = data.filter((item) => item.GMT === value);
+    setNewData(filtered);
+    setAp(true);
+  };
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {data.map((item) => (
+          <div
+            key={item.city}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: "8px",
+            }}
+          >
+            <input type="checkbox" id="wtf" style={{ marginRight: "8px" }} />
+            <label htmlFor="wtf">
+              {item.city} (GMT{item.GMT})
+            </label>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <input type="text" onChange={({ target }) => setValue(target.value)} />
+        <button onClick={button}>Search</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        {ap &&
+          newData.map((item) => (
+            <div key={item.city}>
+              {item.city} (GMT{item.GMT})
+            </div>
+          ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
